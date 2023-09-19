f253e4d4ea6adf80bc8789ec6550a4eebadb8523
Vishnu Pratap Singh <116364032+vpssa@users.noreply.github.com>
2023-09-19 21:21:32 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@ from setuptools import find_packages, setup
 setup(
     name="src",
     version="0.0.1",
-    author="iNeuron",
-    author_email="cloud@ineuron.ai",
+    author="vpssa",
+    author_email="vishnu9109102267@gmail.com",
     packages=find_packages(),
     install_requires=[],
 )
